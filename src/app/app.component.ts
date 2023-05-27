6f9c26cf4f8e91239bf5c2bf03ad975edb2dbb5b
DESKTOP-PO4U83T\sushi <sunil.ladhyal.sharma@gmail.com>
2023-05-28 00:28:29 +0530

    Angular #65 - Reactive Form - Validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,6 @@
 
-
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, NgForm } from '@angular/forms';
-import { HttpRequestService } from './http-request.service';
-
+import { FormControl, FormGroup, NgForm, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -12,32 +9,25 @@ import { HttpRequestService } from './http-request.service';
 
 })
 
-
 export class AppComponent {
 
   loginFormGroup! : FormGroup;
   addrFormGroup: FormGroup = new FormGroup({
-    street : new FormControl(''),
-    city : new FormControl(''),
-    zipcode : new FormControl(''),
+    street : new FormControl('',[Validators.required,Validators.minLength(10),Validators.maxLength(100)]),
+    city : new FormControl('',Validators.required),
+    zipcode : new FormControl('',[Validators.required,Validators.pattern('^[0-9]*$')]),
 
   })
-
   constructor(){
       this.loginFormGroup = new FormGroup({
-        email : new FormControl('ss@test.com'),
-        password : new FormControl('1234'),
+        email : new FormControl('',[Validators.required,Validators.email]),
+        password : new FormControl('',[Validators.required, Validators.minLength(8),Validators.maxLength(10)]),
         address : this.addrFormGroup
       })
   }
 
   login() {
-    const emailMatch = this.loginFormGroup.value.email.split('@')[1];
-    if(emailMatch === 'abc.com') {
-      this.loginFormGroup.removeControl('address');
-    }
-
     console.log("form group : ", this.loginFormGroup, "From Values :", this.loginFormGroup.value);
   }
-
 }
+
